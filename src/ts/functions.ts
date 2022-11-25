79f48a668c6db27c0abd15a8f4abe3ba1dc58b63
Felicia Norman <felicianorman@MacBook-Air-som-tillhor-Felicia.local>
2022-11-25 10:45:27 +0100

    la till kommentarer för functions.ts

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -1,20 +1,25 @@
 import { IAddResponse } from "./models/IAddResult";
 import { Todo } from "./models/Todo";
 
+//Funktion som tillåter oss att addera nya Todos i vår Todo[]
 export function addTodo(todoText: string, todos: Todo[]): IAddResponse {
+  //Om texten är längre än 2 pushar vi todo i listan
   if (todoText.length > 2) {
     let newTodo = new Todo(todoText, false);
     todos.push(newTodo);
     return { success: true, error: "Du måste ange minst två bokstäver" };
+    //Annars vår vi ett felmeddelande
   } else {
     return { success: false, error: "Du måste ange minst två bokstäver" };
   }
 }
 
+//Togglar mellan boolean done från models/Todo
 export function changeTodo(todo: Todo) {
   todo.done = !todo.done;
 }
 
+//Funktion som tar bort alla Todos
 export function removeAllTodos(todos: Todo[]) {
   todos.splice(0, todos.length);
 }
